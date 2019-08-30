96c31e1a5ca8220b3233c6b38ea88c1b1f0b7c9b
HuangQihu <huangqihu@cmdi.chinamobile.com>
2019-08-30 14:23:34 +0800

    创建boot

---
+++ data_tools/src/main/java/com/cmdi/data_tools/DataToolsApplication.java
@@ -0,0 +1,13 @@
+package com.cmdi.data_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataToolsApplication.class, args);
+    }
+
+}
